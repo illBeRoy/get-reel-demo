cd04ab2f26b5d9e40f3b5f6e8f913a7007213e4e
Roy <roy@sommer.co.il>
2024-08-31 20:06:18 +0300

    empty

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,30 +1,5 @@
-import { useEffect, useState } from 'react';
-import { useWixModules } from '@wix/sdk-react';
-import { media } from '@wix/sdk';
-import * as data from '@wix/data';
-import { FeedPage, Reel } from '../../components/FeedPage';
+import { View } from 'react-native';
 
 export default function HomePage() {
-  const wix = useWixModules({ data });
-  const [reels, setReels] = useState<Reel[]>([]);
-
-  useEffect(() => {
-    async function fetchReels() {
-      const results = await wix.data.items
-        .queryDataItems({ dataCollectionId: 'Reels' })
-        .find();
-
-      setReels(
-        results.items.map((result) => ({
-          title: result.data?.title,
-          user: result.data?.user,
-          video: media.getVideoUrl(result.data?.video).url,
-        })),
-      );
-    }
-
-    fetchReels();
-  }, []);
-
-  return <FeedPage reels={reels} />;
+  return <View />;
 }
