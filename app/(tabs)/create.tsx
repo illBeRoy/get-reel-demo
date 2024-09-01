c8d8a2f9a62d66cf1d5840cdd532d7109280cde5
Roy <roy@sommer.co.il>
2024-09-01 14:45:57 +0300

    updated completed code

--- app/(tabs)/create.tsx
+++ app/(tabs)/create.tsx
@@ -1,11 +1,11 @@
+import { View } from 'react-native';
+import { useRouter } from 'expo-router';
 import { IOAuthStrategy } from '@wix/sdk';
 import { useWixAuth, useWixModules } from '@wix/sdk-react';
 import { functions } from '@wix/http-functions';
 import { useWixLogin } from 'wix-login-react-native';
-import { View } from 'react-native';
-import { FileSystemUploadType, uploadAsync } from 'expo-file-system';
 import { CreatePage } from '../../components/CreatePage';
-import { useRouter } from 'expo-router';
+import { uploadVideo } from '../../utils/upload';
 
 export default function Create() {
   const modules = useWixModules({ functions });
@@ -18,24 +18,22 @@ export default function Create() {
     return <View />;
   }
 
-  async function uploadReel(title: string, videoFile: string) {
-    const response = await modules.functions.post('prepareReelUpload', {
-      body: JSON.stringify({ title }),
-    });
-
-    const json = await response.json();
-
-    await uploadAsync(json.uploadUrl, videoFile, {
-      fieldName: 'file',
-      uploadType: FileSystemUploadType.MULTIPART,
-    });
-
-    return 'success' as const;
-  }
-
   return (
     <CreatePage
-      uploadReel={uploadReel}
+      uploadReel={async (title: string, videoFile: string) => {
+        const response = await modules.functions.post('prepareReelUpload', {
+          body: JSON.stringify({ title }),
+        });
+
+        const json = await response.json();
+
+        try {
+          await uploadVideo(videoFile, json.uploadUrl);
+          return 'success';
+        } catch (err) {
+          return 'error';
+        }
+      }}
       onUploadCompleted={() => router.push('/')}
     />
   );
